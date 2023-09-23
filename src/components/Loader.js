a9457ff3f718645b0b3a8173da77d726d29b6931
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2023-09-23 10:45:45 +0530

    commit

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,9 +3,7 @@ import React from 'react'
 const Loader = () => {
     return (
         <div className="w-full flex items-center min-h-screen justify-center">
-            <div className="lds-ripple">
-                <div></div>
-                <div></div>
+            <div className="custom-loader">
             </div>
         </div>
     )
