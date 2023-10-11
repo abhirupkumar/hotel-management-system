96fd316f62b7fe990896266159fe361a82624a55
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2023-10-11 10:18:06 +0530

    commit

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,8 +1,6 @@
 import { SignIn } from "@clerk/clerk-react";
-import { useNavigate } from "react-router-dom";
 
 const SignInPage = () => {
-    const navigate = useNavigate();
 
     return (
         <div className="w-full flex sm:flex-row flex-col items-center min-h-screen justify-center">
@@ -11,7 +9,6 @@ const SignInPage = () => {
             </div>
             <div className="flex flex-col items-center sm:w-1/2 w-full py-10 mb-5">
                 <SignIn afterSignInUrl={process.env.REACT_APP_HOST} signUpUrl={process.env.REACT_APP_HOST + "/sign-up"} />
-                <p onClick={() => navigate("/forgot-password")} className="text-blue-500 hover:text-blue-600 underline cursor-pointer">Forgot Password</p>
             </div>
         </div>
     );
