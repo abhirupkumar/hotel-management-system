7272f399584fde1c9dd9982795fb1c0834da1e93
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2023-10-31 22:37:57 +0530

    commit

--- src/components/PopupMenu.js
+++ src/components/PopupMenu.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import { amber } from "@mui/material/colors";
 import { Link } from "react-router-dom";
 import Table1 from "./Table1";
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+
 function PopupMenu() {
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
     return (
         <>
-            <div className="flex justify-between flex-wrap ">
-                <div className=" flex align-middle w-96 h-10 border border-black mx-3 my-2 ">
+            <div className="flex justify-between items-center flex-wrap ">
+                <div className="flex align-middle flex-1 h-10 border border-black mx-3 my-2 ">
                     <input
                         className=" bg-white border-none flex-1 px-2 text-xs"
                         placeholder="Search for Hotels..."
@@ -19,21 +24,21 @@ function PopupMenu() {
                         </Link>
                     </div>
                 </div>
-                <div className="bg-white h-10 w-36 flex justify-between align-middle p-2 text-xs m-2">
-                    From
-                    <div className="text-amber-500 text-sm cursor-pointer">8/9/2023</div>
-                    <CalendarMonthIcon
-                        className="cursor-pointer"
-                        sx={{ color: amber[500] }}
-                    />
-                </div>
-                <div className="bg-white h-10 w-36 mr-3 flex justify-between p-2 text-xs m-2">
-                    To
-                    <div className="text-amber-500 text-sm cursor-pointer">8/10/2023</div>
-                    <CalendarMonthIcon
-                        className="cursor-pointer"
-                        sx={{ color: amber[500] }}
-                    />
+                <div className="flex justify-between flex-wrap space-x-4 mx-4">
+                    <div className="flex flex-col items-start">
+                        <div className="text-xs bg-zinc-200 z-[10] ml-2">From</div>
+                        <DatePicker className="-mt-2" calendarIcon={<CalendarMonthIcon
+                            className="cursor-pointer h-10"
+                            sx={{ color: "blue" }}
+                        />} calendarAriaLabel="From" value={startDate} onChange={(newValue) => setStartDate(newValue)} />
+                    </div>
+                    <div className="flex flex-col items-start">
+                        <div className="text-xs bg-zinc-200 z-[10] ml-2">To</div>
+                        <DatePicker className="-mt-2" calendarIcon={<CalendarMonthIcon
+                            className="cursor-pointer h-10"
+                            sx={{ color: "blue" }}
+                        />} calendarAriaLabel="To" value={endDate} onChange={(newValue) => setEndDate(newValue)} />
+                    </div>
                 </div>
             </div>
             <Table1 />
